f2b503b3abf3ab54c00b5d39215408e73fa65300
Gabriel Godoy <110994759+oogabrielgodoy@users.noreply.github.com>
2025-10-17 18:51:06 +0000

    atualizando a home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>Home page 🚀</h1>
+    return <div>
+        <h3>Oi meu amor, hoje vou fazer o jantar.</h3>
+        <img src="https://i.gifer.com/7z6p.gif"></img>
+    </div>
 }
 
 export default Home;
